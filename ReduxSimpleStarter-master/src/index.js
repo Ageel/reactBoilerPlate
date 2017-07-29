d1f0dfdc30d709bd4307ffcf37d38bb0bd2ff05c
Ageel <ashatry@gmail.com>
2017-07-29 15:14:04 -0500

    index.js file changes

---
+++ ReduxSimpleStarter-master/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom'
+//Create a new component.  This component should produce some HTML
+
+const App = function() {
+    return <div>Hi!</div>;
+}
+
+//Take this component's generated HTML and put it on the page in the DOM
+ReactDOM.render(App);
